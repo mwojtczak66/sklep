4f34d0b74cc0ae746ae67a863dc7caf9f22b656b
mwojtczak66 <39464978+mwojtczak66@users.noreply.github.com>
2018-05-20 18:21:25 +0000

    sdada

--- formularz.php
+++ formularz.php
@@ -1,12 +1,18 @@
 <?php
-$firstname = "";
-$lastname = "";
-$adress = "";
-$phone = "";
-$username ="";
-$password = "";
 
-$firstname=$_POST["firstname"];
-echo $firstname;
+$db = mysqli_connect('localhost', 'root', '', 'sklep');
 
+$imie = $_POST["imie"];
+$nazwisko = $_POST["nazwisko"];
+$adres = $_POST["adres"];
+$telefon = $_POST["telefon"];
+$login = $_POST["login"];
+$haslo = $_POST["haslo"];	
+
+  	$query = "INSERT INTO uzytkownicy (imie, nazwisko, adres, telefon) VALUES ('$imie', '$nazwisko', '$adres', 'telefon')";
+	$query_second = "INSERT INTO konta (login, haslo) VALUES ('$login', '$haslo')";
+  	mysqli_query($db, $query);
+  	mysqli_query($db, $query_second);
+
+echo $adres;
 ?>
