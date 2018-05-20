1572b4ac582155c22e3b5ec8976c4f472061dfae
mwojtczak66 <39464978+mwojtczak66@users.noreply.github.com>
2018-05-20 18:39:15 +0000

    Working

--- formularz.php
+++ formularz.php
@@ -1,18 +1,43 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="Sklep muzyczny">
+	<link rel="stylesheet" href="muzyka.css">
+ </head>
+ 
+ <body>
+ <div id="main">
+	<div id="baner">
+		<h1>SKLEP MUZYCZNY</h1>
+	</div>
+	<div id="leftpanel">
+		<h2>NASZA OFERTA</h2>
+		<ol>
+			<li>Instrumenty muzyczne</ul>
+			<li>Sprzęt audio</ul>
+			<li>Płyty CD</ul>
+		<ol>	
+	</div>
+</div>
+ </body>
+ </html>
 <?php
 
 $db = mysqli_connect('localhost', 'root', '', 'sklep');
 
-$imie = $_POST["imie"];
-$nazwisko = $_POST["nazwisko"];
-$adres = $_POST["adres"];
-$telefon = $_POST["telefon"];
-$login = $_POST["login"];
-$haslo = $_POST["haslo"];	
+$imie = $_POST["firstname"];
+$nazwisko = $_POST["lastname"];
+$adres = $_POST["adress"];
+$telefon = $_POST["phone"];
+$login = $_POST["username"];
+$haslo = $_POST["password"];	
 
-  	$query = "INSERT INTO uzytkownicy (imie, nazwisko, adres, telefon) VALUES ('$imie', '$nazwisko', '$adres', 'telefon')";
-	$query_second = "INSERT INTO konta (login, haslo) VALUES ('$login', '$haslo')";
-  	mysqli_query($db, $query);
-  	mysqli_query($db, $query_second);
+  	$osobowe = "INSERT INTO uzytkownicy (imie, nazwisko, adres, telefon) VALUES ('$imie', '$nazwisko', '$adres', '$telefon')";
+	$logowania = "INSERT INTO konta (login, haslo) VALUES ('$login', '$haslo')";
+  	mysqli_query($db, $osobowe);
+  	mysqli_query($db, $logowania);
 
-echo $adres;
+  	mysqli_close($db);
+
+  	echo "Konto ", $imie, " ", $nazwisko, " zostało zarejestrowane w sklepie muzycznym";
 ?>
